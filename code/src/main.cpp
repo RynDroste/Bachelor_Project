333ca6e05582576945d281f7a45c9f28812898b0
Xuanlin <chenxu@usi.ch>
2026-03-10 17:48:18 +0800

    Binding the VAO

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -95,9 +95,11 @@ int main() {
          0.0f,  0.5f, 0.0f
     };
 
-    GLuint VBO;
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
 
+    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
@@ -113,6 +115,8 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glfwSwapBuffers(window);
         glfwPollEvents();

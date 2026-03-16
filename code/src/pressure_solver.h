7c4a05413511f08060cff09c803c80a607b16a0c
Xuanlin <chenxu@usi.ch>
2026-03-16 18:20:09 +0800

    Consider the NHSWE

---
+++ code/src/pressure_solver.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+
+class PressureSolver {
+public:
+    PressureSolver(int resolution, float dx, float dy);
+
+    void setDensity(float rhoValue);
+    void setIterations(int maxIters);
+    void setTolerance(float eps);
+
+    float project(std::vector<float>& uField, std::vector<float>& vField, float dt);
+
+    const std::vector<float>& pressure() const;
+
+private:
+    int idxCell(int i, int j) const;
+    int idxU(int i, int jFace) const;
+    int idxV(int iFace, int j) const;
+
+    void buildRhs(
+        const std::vector<float>& uField,
+        const std::vector<float>& vField,
+        float dt
+    );
+    void solvePoisson();
+    void applyPressureGradient(
+        std::vector<float>& uField,
+        std::vector<float>& vField,
+        float dt
+    ) const;
+    float computeMaxAbsDivergence(
+        const std::vector<float>& uField,
+        const std::vector<float>& vField
+    ) const;
+
+    int N;
+    float dx;
+    float dy;
+    float rho;
+    int maxIterations;
+    float tolerance;
+
+    std::vector<float> rhs;
+    std::vector<float> p;
+    std::vector<float> pNext;
+};
+#pragma once
+
+#include <vector>
